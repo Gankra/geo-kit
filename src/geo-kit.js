d51a92f35d2d53360228711214493ec50f2c8c98
Alexis Beingessner <a.beingessner@gmail.com>
2013-03-29 18:53:45 -0400

    fixing up main geokit file

--- src/geo-kit.js
+++ src/geo-kit.js
@@ -1,11 +1,12 @@
-var gk = (function($){
-    var gk = {};
+var gk = (function($, gk){
+    
     $(function(){
         var canvas = document.getElementById("gk-main-canvas");
         var ctx = canvas.getContext("2d");
-        gk.canvas = canvas;
-        gk.ctx = ctx;
+        
+        gk.canvases = [canvas];
+        gk.contexts = [ctx];
     });
 
     return gk;
-})($);
+})($, gk || {});
